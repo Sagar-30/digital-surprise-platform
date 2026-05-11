6753b8e6b62d7dca58590324ba9e4daf91ca8b8a
Sagar-30 <sagar302001@gmail.com>
2026-05-11 21:34:21 +0530

    added under indian santa

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/birthday-surprise">
       <App />
     </BrowserRouter>
   </React.StrictMode>
